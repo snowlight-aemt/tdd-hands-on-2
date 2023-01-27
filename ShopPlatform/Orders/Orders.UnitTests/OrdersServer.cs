3689b91ebd333cb478185d60da86bf6fb68a222a
snowlight-aemt <it.s.aemt@gmail.com>
2023-01-28 00:11:18 +0900

    Test 용 서버를 메모리에 구축

---
+++ ShopPlatform/Orders/Orders.UnitTests/OrdersServer.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Orders;
+
+public sealed class OrdersServer : TestServer
+{
+    private const string ConnectionString = "Server=127.0.0.1;Port=5432;Database=Orders_UnitTests;User Id=postgres;Password=mysecretpassword;";
+        
+    public OrdersServer(
+        IServiceProvider services, 
+        IOptions<TestServerOptions> optionsAccessor) 
+        : base(services, optionsAccessor)
+    {
+        
+    }
+
+    public static OrdersServer Create()
+    {
+        OrdersServer server =  (OrdersServer) new Factory().Server;
+        return server;
+    }
+
+    private sealed class Factory : WebApplicationFactory<Program>
+    {
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // IServer 서비스 타입을 OrdersServer 로 구현한다고 선언함.  
+                services.AddSingleton<IServer, OrdersServer>();
+            });
+
+            builder.ConfigureAppConfiguration(config =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    ["ConnectionStrings:DefaultConnection"] = ConnectionString
+                });
+            });
+            
+            return base.CreateHost(builder);
+        }
+    }
+}
