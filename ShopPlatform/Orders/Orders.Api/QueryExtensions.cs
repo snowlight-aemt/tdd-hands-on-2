9b9d190b52680ab5faf8991a3df2cbb0ae4e66e0
snowlight-aemt <it.s.aemt@email.com>
2023-01-28 15:04:22 +0900

    4. 리팩토링 Code - Order 여러 필터 추가

--- ShopPlatform/Orders/Orders.Api/QueryExtensions.cs
+++ ShopPlatform/Orders/Orders.Api/QueryExtensions.cs
@@ -6,4 +6,14 @@ public static class QueryExtensions
 {
     public static Task<Order?> FindOrder(this DbSet<Order> source, Guid id)
         => source.SingleOrDefaultAsync(x => x.Id == id);
+
+    public static IQueryable<Order> FilterByUser(this IQueryable<Order> source, Guid? userId)
+    {
+        return userId is null ? source : source.Where(x => x.UserId == userId);
+    }
+    
+    public static IQueryable<Order> FilterByShop(this IQueryable<Order> source, Guid? shopId)
+    {
+        return shopId is null ? source : source.Where(x => x.ShopId == shopId);
+    }
 }
