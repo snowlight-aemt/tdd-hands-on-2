843777bb42f8c6ed80586e78ad9358ea45bd5ce0
snowlight-aemt <it.s.aemt@gmail.com>
2023-01-30 21:31:21 +0900

    비동기 프로세스 추가 (Message Query)

--- ShopPlatform/Orders/Orders.Api/Order.cs
+++ ShopPlatform/Orders/Orders.Api/Order.cs
@@ -20,6 +20,8 @@ public sealed class Order
     public decimal Price { get; set; }
 
     public OrderStatus Status { get; set; }
+    
+    public string PaymentTransactionId { get; set; }
 
     public DateTime PlacedAtUtc { get; set; }
 
