65445597ce81bcb1896c92c7d45bf1fec14d3420
snowlight-aemt <it.s.aemt@email.com>
2023-02-01 23:25:14 +0900

    1. Accounting - Refactoring Code : Order Status 체크

--- ShopPlatform/accounting/unittest/src/test/java/accounting/test/OrderViewAggregator_specs.java
+++ ShopPlatform/accounting/unittest/src/test/java/accounting/test/OrderViewAggregator_specs.java
@@ -26,39 +26,7 @@ public class OrderViewAggregator_specs {
         var sut = new OrderViewAggregator(shopReader);
 
         // 세터가 없음.
-        Order order = new Order();
-        try {
-            Field id = Order.class.getDeclaredField("id");
-            id.setAccessible(true);
-            id.set(order, UUID.randomUUID());
-
-            Field userId = Order.class.getDeclaredField("userId");
-            userId.setAccessible(true);
-            userId.set(order, UUID.randomUUID());
-
-            Field shopId = Order.class.getDeclaredField("shopId");
-            shopId.setAccessible(true);
-            shopId.set(order, UUID.randomUUID());
-
-            Field itemId = Order.class.getDeclaredField("itemId");
-            itemId.setAccessible(true);
-            itemId.set(order, UUID.randomUUID());
-
-            Field price = Order.class.getDeclaredField("price");
-            price.setAccessible(true);
-            price.set(order, new BigDecimal(100000));
-
-            Field status = Order.class.getDeclaredField("status");
-            status.setAccessible(true);
-            status.set(order, "Pending");
-
-            Field placedAtUtc = Order.class.getDeclaredField("placedAtUtc");
-            placedAtUtc.setAccessible(true);
-            placedAtUtc.set(order, LocalDateTime.now());
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        Order order = getOrder("Pending");
 
         // act
         Iterable<OrderView> orderViews = sut.aggregateViews(List.of(order));
@@ -70,45 +38,13 @@ public class OrderViewAggregator_specs {
     }
 
     @Test
-    void sut_localizes_AwaitPayment_status() {
+    void sut_localizes_AwaitingPayment_status() {
         // arrange
         ShopReader shopReader = id -> Optional.empty();
         var sut = new OrderViewAggregator(shopReader);
 
         // 세터가 없음.
-        Order order = new Order();
-        try {
-            Field id = Order.class.getDeclaredField("id");
-            id.setAccessible(true);
-            id.set(order, UUID.randomUUID());
-
-            Field userId = Order.class.getDeclaredField("userId");
-            userId.setAccessible(true);
-            userId.set(order, UUID.randomUUID());
-
-            Field shopId = Order.class.getDeclaredField("shopId");
-            shopId.setAccessible(true);
-            shopId.set(order, UUID.randomUUID());
-
-            Field itemId = Order.class.getDeclaredField("itemId");
-            itemId.setAccessible(true);
-            itemId.set(order, UUID.randomUUID());
-
-            Field price = Order.class.getDeclaredField("price");
-            price.setAccessible(true);
-            price.set(order, new BigDecimal(100000));
-
-            Field status = Order.class.getDeclaredField("status");
-            status.setAccessible(true);
-            status.set(order, "AwaitingPayment");
-
-            Field placedAtUtc = Order.class.getDeclaredField("placedAtUtc");
-            placedAtUtc.setAccessible(true);
-            placedAtUtc.set(order, LocalDateTime.now());
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        Order order = getOrder("AwaitingPayment");
 
         // act
         Iterable<OrderView> orderViews = sut.aggregateViews(List.of(order));
@@ -117,5 +53,27 @@ public class OrderViewAggregator_specs {
         List<OrderView> list = new ArrayList<>();
         orderViews.forEach(list::add);
         Assertions.assertEquals("결제대기", list.get(0).status());
+    }
+
+    private static Order getOrder(String status) {
+        Order order = new Order();
+        setField(order, "id", UUID.randomUUID());
+        setField(order, "userId", UUID.randomUUID());
+        setField(order, "shopId", UUID.randomUUID());
+        setField(order, "itemId", UUID.randomUUID());
+        setField(order, "price", new BigDecimal(100000));
+        setField(order, "status", status);
+        setField(order, "placedAtUtc", LocalDateTime.now());
+        return order;
+    }
+
+    private static void setField(Order order, String name, Object value) {
+        try {
+            Field id = Order.class.getDeclaredField(name);
+            id.setAccessible(true);
+            id.set(order, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
