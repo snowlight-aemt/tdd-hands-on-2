fbc23457e9da6016264a0451324a03c10acc5906
snowlight-aemt <it.s.aemt@gmail.com>
2023-02-08 21:53:31 +0900

    Test Code - UserId and PasswordHash does not expose

--- ShopPlatform/Sellers/Sellers.UnitTests/api/shops/id/Get_specs.cs
+++ ShopPlatform/Sellers/Sellers.UnitTests/api/shops/id/Get_specs.cs
@@ -1,11 +1,37 @@
 using System.Net.Http.Json;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Sellers.api.shops.id;
 
 public class Get_specs
 {
+    [Fact]
+    public async Task Sut_does_not_expose_user_credentials()
+    {
+        SellersServer server = SellersServer.Create();
+
+        IServiceScope scope = server.Services.CreateScope();
+        SellersDbContext context = scope.ServiceProvider.GetRequiredService<SellersDbContext>();
+        Shop shop = new()
+        {
+            Id = Guid.NewGuid(),
+            Name = $"{Guid.NewGuid()}",
+            UserId = $"{Guid.NewGuid()}",
+            PasswordHash = $"{Guid.NewGuid()}",
+        };
+        context.Shops.Add(shop);
+        await context.SaveChangesAsync();
+
+        HttpClient client = server.CreateClient();
+        HttpResponseMessage response = await client.GetAsync($"api/shops/{shop.Id}");
+        Shop? actual = await response.Content.ReadFromJsonAsync<Shop>();
+
+        actual!.UserId.Should().BeNull();
+        actual.PasswordHash.Should().BeNull();
+    }
+    
     [Fact]
     public async Task Sut_does_not_expose_user_information()
     {
