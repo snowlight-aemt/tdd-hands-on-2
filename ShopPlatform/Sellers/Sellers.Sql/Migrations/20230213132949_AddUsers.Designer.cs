d0328f51127dfc7ae58d8dbf8d13b5c33475fe75
snowlight-aemt <it.s.aemt@gmail.com>
2023-02-13 22:41:32 +0900

    Data Model User (DbContext 추가)

--- ShopPlatform/Sellers/Sellers.Sql/Migrations/20230213132949_AddUsers.Designer.cs
+++ ShopPlatform/Sellers/Sellers.Sql/Migrations/20230213132949_AddUsers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sellers;
@@ -11,9 +12,10 @@ using Sellers;
 namespace Sellers.Migrations
 {
     [DbContext(typeof(SellersDbContext))]
-    partial class SellersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230213132949_AddUsers")]
+    partial class AddUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -72,7 +74,6 @@ namespace Sellers.Migrations
                         .HasColumnType("text");
 
                     b.Property<string>("Username")
-                        .IsRequired()
                         .HasColumnType("text");
 
                     b.HasKey("Sequence");
--- ShopPlatform/Sellers/readme.md
+++ ShopPlatform/Sellers/readme.md
@@ -1,4 +1,7 @@
 ### 데이터베이스 생성
 ```shell
-dotnet ef database update -v 
+dotnet ef database update -v
+
+dotnet ef migrations add AddUsers --project Sellers.Sql --startup-project ./Sellers.Api
+dotnet ef database update --project Sellers.Sql --startup-project Sellers.Api 
 ```
