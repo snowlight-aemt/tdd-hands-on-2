ddf02ce0a89abd8095fd6fb34064748ce6af70a8
snowlight-aemt <it.s.aemt@gmail.com>
2023-02-18 15:15:40 +0900

    Test Code 3-1 Create User Command Executor

--- ShopPlatform/Sellers/Sellers.DomainModel/CommandModel/CreateUserCommandExecutor.cs
+++ ShopPlatform/Sellers/Sellers.DomainModel/CommandModel/CreateUserCommandExecutor.cs
@@ -5,15 +5,37 @@ namespace Sellers.CommandModel;
 
 public class CreateUserCommandExecutor
 {
+    private readonly IUserReader reader;
+    private readonly IPasswordHasher hasher;
+    private readonly IUserRepository repository;
+
     public CreateUserCommandExecutor(
         IUserReader reader,
         IPasswordHasher hasher,
         IUserRepository repository)
     {
+        this.reader = reader;
+        this.hasher = hasher;
+        this.repository = repository;
     }
 
-    public Task Execute(Guid id, CreateUser command)
+    public async Task Execute(Guid id, CreateUser command)
     {
-        return Task.CompletedTask;
+        await AssertThatUsernameIsUnique(command.Username);
+        await AddUser(id, command);
+    }
+
+    private Task AddUser(Guid id, CreateUser command)
+    {
+        string hashPassword = this.hasher.HashPassword(command.Password);
+        return this.repository.Add(new User(id, command.Username, hashPassword));
+    }
+
+    private async Task AssertThatUsernameIsUnique(string username)
+    {
+        if (await this.reader.FindUser(username) is not null)
+        {
+            throw new InvariantViolationException();
+        }
     }
 }
