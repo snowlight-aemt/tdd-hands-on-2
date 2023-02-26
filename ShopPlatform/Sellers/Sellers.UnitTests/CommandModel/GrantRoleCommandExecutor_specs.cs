d68846040a180a7cc85b2d887da34e4f0d96ff77
snowlight-aemt <it.s.aemt@gmail.com>
2023-02-26 12:36:56 +0900

    Role 이 있는 상태에서 새로 Role 추가 - 이슈존재

--- ShopPlatform/Sellers/Sellers.UnitTests/CommandModel/GrantRoleCommandExecutor_specs.cs
+++ ShopPlatform/Sellers/Sellers.UnitTests/CommandModel/GrantRoleCommandExecutor_specs.cs
@@ -9,7 +9,7 @@ namespace Sellers.CommandModel;
 public class GrantRoleCommandExecutor_specs
 {
     [Theory, AutoSellersData]
-    public async Task Sut_currently_add_role(
+    public async Task Sut_currently_adds_role(
         GrantRoleCommandExecutor sut,
         Func<SellersDbContext> contextFactory,
         GrantRole command,
@@ -24,5 +24,24 @@ public class GrantRoleCommandExecutor_specs
         User actual = (await new SqlUserReader(contextFactory).FindUser(user.Id))!;
         actual.Roles.Should().NotBeEmpty();
         actual.Roles.Should().Contain(new Role(command.ShopId, command.RoleName));
+    }
+
+    public async Task Sut_currently_appends_role(
+        GrantRoleCommandExecutor sut,
+        Func<SellersDbContext> contextFactory,
+        GrantRole command1,
+        GrantRole command2,
+        UserEntity user)
+    {
+        SellersDbContext context = contextFactory.Invoke();
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        await sut.Execute(user.Id, command1);
+        
+        await sut.Execute(user.Id, command2);
+
+        User actual = (await new SqlUserReader(contextFactory).FindUser(user.Id))!;
+        actual.Roles.Should().Contain(new Role(command1.ShopId, command1.RoleName));
+        actual.Roles.Should().Contain(new Role(command2.ShopId, command2.RoleName));
     }
 }
